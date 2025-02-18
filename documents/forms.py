8645c3df4ac00d6d796108acfb40aababe164e12
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-18 20:57:53 +0530

    commit

--- documents/forms.py
+++ documents/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Document, Category
-from django.forms.widgets import FileInput
 
 class DocumentForm(forms.ModelForm):
     class Meta:
@@ -23,45 +22,23 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class MultipleFileInput(FileInput):
-    def __init__(self, attrs=None):
-        default_attrs = {'multiple': 'multiple'}
-        if attrs:
-            default_attrs.update(attrs)
-        super().__init__(default_attrs)
-
-class MultipleFileField(forms.FileField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
-
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = single_file_clean(data, initial)
-        return result
-
 class BatchUploadForm(forms.Form):
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(),
         widget=forms.Select(attrs={'class': 'form-control'}),
         label='Category'
     )
-    files = MultipleFileField(
-        widget=MultipleFileInput(attrs={
+    files = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={
             'class': 'form-control',
+            'multiple': True,
             'accept': '.pdf,.doc,.docx,.txt,.xls,.xlsx'
         }),
-        help_text='Hold Ctrl/Cmd to select multiple files.',
         label='Select Files'
     )
     is_private = forms.BooleanField(
         required=False,
         initial=False,
-        widget=forms.CheckboxInput(attrs={
-            'class': 'form-check-input'
-        }),
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         label='Make Private'
     )
